84f81cba99fafa7b81ec822bfbee8992f6e3f7f7
rhruban <rhruban@gmail.com>
2025-05-21 08:23:29 -0600

    set the frame rate

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ def main():
     print("Starting Asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
+
